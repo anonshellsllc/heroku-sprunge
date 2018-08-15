3c3d08ca3e8ed4ec957f055889f11afddc40bcc8
anonshellsllc <40561508+anonshellsllc@users.noreply.github.com>
2018-08-15 14:48:50 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #/usr/bin/env bash
 
-export SPRUNGE_HOST='http://localhost:8080'
+export SPRUNGE_HOST='http://127.0.0.1:8080'
 export SPRUNGE_NAME='sprunge'
-export SPRUNGE_MONGO_URI=''
-export SPRUNGE_MONGO_DB_NAME=''
+export SPRUNGE_MONGO_URI='127.0.0.1'
+export SPRUNGE_MONGO_DB_NAME='sprunge'
 
 python sprunge/app.py
